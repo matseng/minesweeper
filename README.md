faf9dabd5ea80c0dd383d7947c28bee90d9ca13e
Michael Tseng <matseng@gmail.com>
2014-06-21 19:52:46 -0700

    Initial message

---
+++ README.md
@@ -0,0 +1,12 @@
+README.md
+# matseng: To get set up for a new project, from the command line run:
+  #  $ cp -r angularBootstrapTemplate myNewProjectName
+  #  $ rm -rf .git  //removes previous git commits from the template repo
+  #  $ git init
+  #  $ git add .
+  #  $ git commit -m "Initial commit"
+  #  $ ./gitcreate-v03.sh
+
+# Optional: To install angular-animate:
+  #bower install angular-animate -S
+  #Note: By default, angular-animate has already been included in .gitignore and index.html
---
+++ src/app.js
@@ -0,0 +1,93 @@
+angular.module('MyApp', []);
+
+angular.module('MyApp')
+  .controller('MyController', ['$scope', function($scope) {
+
+    var Tile = function(i, j) {
+      this.i = i;
+      this.j = j;
+      this.mine = null;
+      this.adjacentMines = null;
+    };
+
+    var Board = function(n, numberOfMines) {
+      this.n = n || 8;
+      this.numberOfMines = numberOfMines || 10;
+      this.board = [];
+    };
+
+    Board.prototype.initializeBoard = function() {
+      var row, n;
+      n = this.n;
+      for(var i = 0; i < n; i++) {
+        row = [];
+        for(var j = 0; j < n; j++) {
+          row[j] = new Tile(i, j);
+        }
+        this.board.push(row);
+      }
+    };
+
+    Board.prototype.addMines = function() {
+      var i, j, n;
+      n = this.n;
+      for(var k = 0; k < this.numberOfMines; k++) {
+        i = getRandomIndex();
+        j = getRandomIndex();
+        while (this.board[i][j].mine === true) {
+          i = getRandomIndex();
+          j = getRandomIndex();
+        }
+        this.board[i][j].mine = true;
+        console.log(i,j,this.board[i][j].mine);
+      }
+
+      function getRandomIndex() {
+        return index = Math.floor(Math.random() * (n));
+      };
+    };
+
+    Board.prototype.countAdjacentMines = function() {
+      var i, j;
+      for(var i = 0; i < this.n; i++) {
+        for(var j = 0; j < this.n; j++) {
+          countAdjacentMinesPerTile.bind(this)(i, j);
+        }
+      }
+
+      function countAdjacentMinesPerTile(x, y) {
+        var count, tile;
+        count = 0;
+        for(var i = x - 1; i <= x + 1; i++) {
+          for(var j = y - 1; j <= y + 1; j++) {
+            if(i >= 0 && j >= 0 && i < this.n && j < this.n) {
+              tile = this.board[i][j];
+              if(tile.mine === true) {
+                count += 1;
+              }
+            }
+          }
+        }
+        this.board[x][y].adjacentMines = count;
+      };
+
+    };
+
+    $scope.tileClicked = function(i,j) {
+      var tile = $scope.board.board[i][j];
+      console.log(i,j, 'mine:' + tile.mine);
+    };
+
+    (function initialize() {
+      var n = 8;
+      var numberOfMines = 10;
+      var board = new Board(n, numberOfMines)
+      board.initializeBoard();
+      console.log(board);
+      board.addMines(numberOfMines)
+      board.countAdjacentMines();
+      $scope.board = board;
+      console.log($scope.board);
+    })();
+
+  }]);
