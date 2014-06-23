edb371f7e54ece8b9109ba1cadd57a100a705992
Michael Tseng <matseng@gmail.com>
2014-06-23 09:13:08 -0700

    Fixed bug with templateUrl (NOT templateURL)

--- src/modules/minesweeper.js
+++ src/modules/minesweeper.js
@@ -5,12 +5,8 @@ angular.module('minesweeper')
   .directive('minesweeperDirective', [function() {
     return {
       restrict: 'E',
-      // replace:'true',
       controller: 'MyController',
-      template: '<div>hello world </div>',
-      // templateURL: 'main.html'
-      // templateURL: '/modules/main.html'
-      // templateURL: 'src/modules/main.html',
+      templateUrl: './src/modules/main.html',
       // scope: {selection: '='}
       // controller: function($scope, $element){},
       link: link
